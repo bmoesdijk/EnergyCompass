5d94cfd4e95650e42c42ee60837665ff339e53d1
Boudewijn van de Moesdijk <boudewijn@moesdijk.com>
2021-12-06 14:28:44 +0100

    Update p1_webserver_py3.py

--- P1_Interface/p1_webserver_py3.py
+++ P1_Interface/p1_webserver_py3.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python
 #!/volume1/@appstore/py3k/usr/local/bin/python3
+#DSMR P1 smartmeter data retreival
+#Date: 11-may-2020
+#version: 1.0 
+#Author: B. van de Moesdijk
+#License: 
+"""	This file is part of EnergyCompas.
+    EnergyCompass is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+    EnergyCompass is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with EnergyCompass.  If not, see <http://www.gnu.org/licenses/>.
+"""
 import sys
 import http.server
 import time
